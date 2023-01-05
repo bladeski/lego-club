e0dc828df963131dd6bf5778766758dcfac0f224
Jonathan Blades <jonathan.blades@ibigroup.com>
2023-01-05 12:29:26 +0000

    Updated styling

--- src/scripts/Challenges.ts
+++ src/scripts/Challenges.ts
@@ -1,10 +1,5 @@
 const Challenges: ChallengeModel[] = [
-  {
-    title: 'January 2023',
-    challengeText: 'This is a challenge',
-    length: [0, 45, 0],
-    date: '2023-01-05'
-  }
+
 ]
 
 export default Challenges;
