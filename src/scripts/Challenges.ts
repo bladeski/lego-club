bfc9062f096bf1276c76b1e4e106655c6509eaa1
Jonathan Blades <jonathan.blades@ibigroup.com>
2023-02-06 09:22:11 +0000

    Fix: issue with font size being too large on smaller screens and updated February challenge.

--- src/scripts/Challenges.ts
+++ src/scripts/Challenges.ts
@@ -6,11 +6,17 @@ const Challenges: ChallengeModel[] = [
     date: '2023-01-14'
   },
   {
-    title: 'Going travelling...',
-    challengeText: 'With help from your grown-up, build somewhere that you would like to visit... even if it hasn\'t been discovered yet!',
+    title: 'It must be love!',
+    challengeText: 'Build something to do with love. It could be someone or something you love, or that makes you think of love!',
     length: [0, 45, 0],
     date: '2023-02-11'
   },
+  // {
+  //   title: 'Going travelling...',
+  //   challengeText: 'With help from your grown-up, build somewhere that you would like to visit... even if it hasn\'t been discovered yet!',
+  //   length: [0, 45, 0],
+  //   date: '2023-02-11'
+  // },
   {
     title: 'Creeping and Crawling',
     challengeText: 'With help from your grown-up, build a bug! It could be an insect, a spider, or something from your imagination!',
